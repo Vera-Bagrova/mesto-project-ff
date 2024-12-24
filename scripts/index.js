8b74a6c4a380671559b4bdf1b8fd1a0ec3521402
Vera Bagrova <bagrovavg@yandex.ru>
2024-12-24 21:04:16 +0300

    fixed

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,21 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 const content = document.querySelector('.content');
-const placesList = content.querySelector('.places__list');
+const cardsContainer = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 function createCard(cardData, onDelete) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
   const deleteButton = cardElement.querySelector('.card__delete-button');
 
-  // присваиваем значения элементов клонированного шаблона
-  cardElement.querySelector('.card__image').src = cardData.link;
-  cardElement.querySelector('.card__image').alt = cardData.name;
-  cardElement.querySelector('.card__title').textContent = cardData.name;
+  /**  присваиваем значения элементов клонированного шаблона */
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
 
-  // обработчик клика иконки удаления, по которому будет вызван переданный в аргументах колбэк
+  /** обработчик клика иконки удаления, по которому будет вызван переданный в аргументах колбэк */
   deleteButton.addEventListener('click', () => {
     onDelete(cardElement);
   });
@@ -32,5 +34,5 @@ function deleteCard(card) {
 initialCards.forEach((elem) => {
   const card = createCard(elem, deleteCard);
 
-  placesList.append(card);
+  cardsContainer.append(card);
 });
