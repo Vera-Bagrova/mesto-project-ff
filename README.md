d6d9c2fdfe13e704f1a5dcdc178712e62e5697a1
Vera Bagrova <bagrovavg@yandex.ru>
2024-12-24 16:43:29 +0300

    create_delete_add_card

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Vera-Bagrova/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, onDelete) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  // присваиваем значения элементов клонированного шаблона
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+
+  // обработчик клика иконки удаления, по которому будет вызван переданный в аргументах колбэк
+  deleteButton.addEventListener('click', () => {
+    onDelete(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(card) {
+  card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((elem) => {
+  const card = createCard(elem, deleteCard);
+
+  placesList.append(card);
+});
